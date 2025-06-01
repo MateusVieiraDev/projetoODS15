5b96fe3208658e5028610c9073e3ebf9642b5aa9
Mateus <mateusdabruna2008@gmail.com>
2025-06-01 16:33:22 -0300

    to sem nada pra fazer, atualizei

---
+++ front-end/welcome-page/script.js
@@ -0,0 +1,6 @@
+const botao = document.getElementById("botao-cadastro");
+
+function levarAoCadastro() {
+    window.location.href = "cadastro.html";
+}
+botao.addEventListener("click", levarAoCadastro);
